053cab948a276402ac3079c461d5720e3b11df24
wfortj <trebrodrigo@gmail.com>
2023-04-24 22:43:23 +0800

    first commit

---
+++ src/appendWeather.js
@@ -0,0 +1,32 @@
+import kelvinToCelsius from "./utils/kelvinToCelsius";
+import titleCase from "./utils/titleCase";
+import updateTime from "./updateTime";
+
+export default function appendWeather(weatherData) {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  const h1 = document.createElement("h1");
+  h1.textContent = `${weatherData.name}`;
+  const p = document.createElement("p");
+  p.classList.add("description");
+  p.textContent = titleCase(`${weatherData.weather[0].description}`);
+
+  const main = document.createElement("main");
+
+  const iconContainer = document.createElement("div");
+  const weatherIcon = document.createElement("img");
+  weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+
+  const tempContainer = document.createElement("div");
+  const temp = document.createElement("h2");
+
+  temp.textContent = `${kelvinToCelsius(weatherData.main.temp)}°C`;
+
+  const detailsContainer = document.createElement("div");
+
+  iconContainer.appendChild(weatherIcon);
+  tempContainer.appendChild(temp);
+  main.append(h1, p, iconContainer, tempContainer, detailsContainer);
+  app.append(main);
+}
